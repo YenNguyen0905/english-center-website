e1dae5a862f9a25e3b55a8e6b68799ef28425f7b
Yen Nguyen <yenn2k19@gmail.com>
2020-12-29 02:19:02 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Show navbar list when click to responsive button
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
